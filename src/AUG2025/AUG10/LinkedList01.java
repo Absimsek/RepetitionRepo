ade01f47ea26e3cafd09bf9832d2f9b1d2ddd507
Absimsek <aliberkaysimsekk@gmail.com>
2025-07-11 15:12:24 +0300

    JULY10

--- src/AUG2025/AUG10/LinkedList01.java
+++ src/AUG2025/AUG10/LinkedList01.java
@@ -1,5 +1,7 @@
 package AUG2025.AUG10;
 
+import java.util.LinkedList;
+
 public class LinkedList01 {
     // until here for data sets we used arrays and arraylists
     // Arrays are fast for search but static memory size so no adding or deleting more than max is possible
@@ -20,5 +22,41 @@ public class LinkedList01 {
     // NEED TO         fast access data              fast access data             frequently use add/delete
 
     // SIZE           static non-changeable             dynamic-fixed                  dynamic-fixed
+
+
+
+
+    public static void main(String[] args) {
+    LinkedList<String> l1=new LinkedList<>();// LinkedList creation
+        l1.add("istanbul");
+        l1.add("Ankara");
+        l1.add("Kırıkkale");
+        l1.add("Sivas");
+        l1.add("Samsun");
+        System.out.println(l1);
+
+        System.out.println(l1.remove(3));// remove by index
+        System.out.println(l1.remove());// removes index 0 so head node
+        System.out.println(l1.remove("kırıkkale"));//removes specified string
+
+        // if there are more than 1 same strings than removeFirstOccurrence or lastOccurrence would work to specify which one to remove
+        l1.add("a");
+        l1.add("b");
+        l1.add("c");
+        l1.add("d");
+
+        System.out.println(l1);
+        // to return the head node of linkedlist use peek()
+        System.out.println(l1.peek());
+        System.out.println(l1);
+        // to both return and remove head node use poll()
+        System.out.println(l1.poll());
+        System.out.println(l1);
+
+        //element() is same as peek but when list empty error occurs
+        //pop() is same as poll but when list empty error occurs
+
+
+    }
 
 }
