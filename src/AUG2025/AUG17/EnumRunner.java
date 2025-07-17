70a70f52c8f63e253ae888e371a5dfaac84c9241
Absimsek <aliberkaysimsekk@gmail.com>
2025-07-17 11:12:11 +0300

    Reapply "july17"
    This reverts commit a6517f73d384e30fd5c04fc697e4ffad61d11893.

---
+++ src/AUG2025/AUG17/EnumRunner.java
@@ -0,0 +1,16 @@
+package AUG2025.AUG17;
+
+public class EnumRunner {
+    public static void main(String[] args) {
+
+        Names berkay=Names.BERKAY;
+        System.out.println(berkay);
+
+        String majorOfBerkay=Names.BERKAY.getMajor();
+        System.out.println(majorOfBerkay);
+
+
+
+
+    }
+}
---
+++ src/AUG2025/AUG17/InvalidStudentGradeException.java
@@ -0,0 +1,11 @@
+package AUG2025.AUG17;
+
+public class InvalidStudentGradeException extends Exception{
+
+    public InvalidStudentGradeException(String message){
+        super(message);
+    }
+
+
+
+}
